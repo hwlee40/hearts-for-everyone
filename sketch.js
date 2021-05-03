160c07ccb2c848256cadeca24ad4c26386ef4666
Hwan Lee <hlee40@Hwanui-MacBookPro.local>
2021-05-03 10:13:23 +0900

    adjusted canvas size to be flexible + styling changes

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
-
+//Global variables
 let video;
 let classifier;
 let label;
-
+let directions;
 let isWebcamReady = false;
 let stepSize = 12;
+let desiredWidth, desiredHeight;
 
+//Loads teachable machine into ml5 before executing any other script
 function preload(){
-  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/NswGtsSoL/model.json');
+  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/bVqnRzx4f/model.json');
 }
 
 function setup() {
@@ -59,81 +61,39 @@ function draw() {
           }
         }
       }
-    }
+    };
+
+    directions = '🤍💛💚💙💜❤️';
   } else {
+    //Webcam
     video.loadPixels();
-    image(video, 0,0, windowWidth, windowWidth * 0.75);
+    image(video, 0,0, desiredWidth, desiredHeight);
+    directions = 'Raise your hand for some love :)';
   }
+
+  //Some text for directions. Directions var changes depending on label state in the if else statement above.
+  textSize(24);
+    textAlign(CENTER, CENTER);
+    fill(255);
+    text(directions, width / 2, height - 24);
 
 }
 
+//If input is unrecognized, return error. Change label var to name of classification and call the classifyVideo() each time.
 function gotResults(error, results) {
   if (error) {
     console.error(error);
     return;
   }
-  console.log(results[0].label);
+  //console.log(results[0].label);
   label = results[0].label;
   classifyVideo();
 }
 
+//Scale pixel video & webcam proportionally.
 function scaleCanvasToCapture() {
-  video.size(windowWidth, windowHeight); 
-}
-
-/* let capture;
-let desiredWidth, desiredHeight;
-let isWebcamReady = false;
-let stepSize = 15;
-
-function preload() {
-  createCanvas(500, 500);
-  // Create webcam capture
-  capture = createCapture(VIDEO, function(){
-    scaleCanvasToCapture();
-    isWebcamReady = true;
-  });
-  capture.hide();
-}
-
-function draw() {
-  background(255);
-  if (isWebcamReady){
-    capture.loadPixels();
-    // Iterates through pixels in capture, in intervals of 8
-    for (let y = 0; y < capture.height; y += stepSize) {
-      for (let x = 0; x < capture.width; x += stepSize) {
-        let indexOfRed = (x + y * capture.width) * 4;
-        // Gets color of current pixel
-        let r = capture.pixels[indexOfRed];
-        let g = capture.pixels[indexOfRed + 1];
-        let b = capture.pixels[indexOfRed + 2];
-        let a = capture.pixels[indexOfRed + 3];
-        // Gets the brightness of the pixel
-        let brightness_val = brightness([r, g, b, a]);
-        textSize(stepSize - 1);
-        // Draws an emoji here given the brightness value
-        if (brightness_val > 70){
-          text('💡', x, y);
-        } else if (brightness_val > 45){
-          text('🍏', x, y);
-        } else if (brightness_val > 20) {
-          text('💩', x, y);
-        } else {
-          text('💣', x, y);
-        }
-      }
-    }
-  }
-}
-
-function scaleCanvasToCapture() {
-  // Sets desired width of canvas to width of the window
-  desiredWidth = windowWidth/1.5; 
-  // Calculates height according to webcam feed's scale
-  desiredHeight = windowWidth * (capture.height / capture.width)/1.5; 
-  // Resizes the canvas to the desired dimensions
+  desiredWidth = windowHeight * (video.width / video.height) * 0.9; 
+  desiredHeight = windowHeight * 0.9; 
   resizeCanvas(desiredWidth, desiredHeight); 
-  // Uses the .size() method to resize original webcam capture element
-  capture.size(desiredWidth, desiredHeight); 
-} */
+  video.size(desiredWidth, desiredHeight); 
+}
